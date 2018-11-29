3732d71867935ad7c9807503d212edad865609fd
David Olah <david.olah@cofinpro.de>
2018-11-29 13:43:51 +0100

    Initial set up of maven module. Added co#sad file

---
+++ praxisphase3-acsd-dependencies/src/main/java/de/cofinpro/melise/praxisphase3/Application.java
@@ -0,0 +1,8 @@
+package de.cofinpro.melise.praxisphase3;
+
+/**
+ * Created by David Olah, Cofinpro AG.
+ * Date: 29.11.2018.
+ */
+public class Application {
+}
---
+++ praxisphase3-acsd-dependencies/src/test/java/de/cofinpro/melise/praxisphase3/ApplicationTest.java
@@ -0,0 +1,17 @@
+package de.cofinpro.melise.praxisphase3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by David Olah, Cofinpro AG.
+ * Date: 29.11.2018.
+ */
+public class ApplicationTest {
+    @Test
+    public void testWorks() {
+        assertEquals(10, 10);
+    }
+
+}
